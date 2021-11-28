774bc70eae73a1b3f1df35173b98b5ebc84a8f0f
jamesmwangi254 <james.kariuki@student.moringaschool.com>
2021-11-28 17:56:57 +0300

    first commit

---
+++ Pizzainn/script.js
@@ -0,0 +1,35 @@
+$('.imageshow').hide();
+if($('.btn-click').on('click', function(){
+    $('.imageshow').show();
+  
+    if($('.btn-click').on('click', function(){
+        $('.imageshow').hide();
+      
+    }));
+    
+
+}));
+
+
+    $('.imageshow').show();
+if($('.btn-click').on('click', function(){
+    $('.imageshow').hide();
+  
+    if($('.btn-click').on('click', function(){
+        $('.imageshow').show();
+      
+    }));
+    
+    
+
+}));
+
+
+    
+
+
+
+if($('.btn-click').on('click', function(){
+    $('.imageshow').hide();
+  
+}));
