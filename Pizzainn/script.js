d7f89b8a225ccd280c8a1f13721206bda8915d35
jamesmwangi254 <james.kariuki@student.moringaschool.com>
2021-11-30 12:24:56 +0300

    New changes to the prices of the order

--- Pizzainn/script.js
+++ Pizzainn/script.js
@@ -1,35 +1,10 @@
-$('.imageshow').hide();
-if($('.btn-click').on('click', function(){
-    $('.imageshow').show();
-  
-    if($('.btn-click').on('click', function(){
-        $('.imageshow').hide();
-      
-    }));
-    
-
-}));
 
 
-    $('.imageshow').show();
-if($('.btn-click').on('click', function(){
-    $('.imageshow').hide();
-  
-    if($('.btn-click').on('click', function(){
-        $('.imageshow').show();
-      
-    }));
-    
-    
-
-}));
+$('.btn-click').click(function () {
 
 
-    
+    $(".imageshow").toggle()
+});
 
 
 
-if($('.btn-click').on('click', function(){
-    $('.imageshow').hide();
-  
-}));
